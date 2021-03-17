a3751de11766322ec20a6ebd2fb056afbea7a89f
Anouar <41291745+fun-ux@users.noreply.github.com>
2021-03-18 00:12:53 +0100

    Update fastingquiz.js

--- fastingquiz.js
+++ fastingquiz.js
@@ -39,7 +39,7 @@ $(document).ready(function () {
         
        /* alert($('#new_back').val());
                 alert('fsa');*/
-        $('#old_back >  .button_wrappler  > .back-button').addClass("back-button-disabled");
+        $($('#old_back').attr('value')+' >  .button_wrappler  > .back-button').addClass("back-button-disabled");
     }); 
 
     //fades in out
